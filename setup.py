97434cac4b966ca1cb93268529ac9de2bb7a0c8a
chip <chip@codime.io>
2024-08-06 15:13:15 +0900

    Update files for package

---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+
+install_requires = [
+    'pillow', 
+    'tqdm', 
+    'Ninja', 
+    'opencv-python', 
+    'torch', 
+    'torchvision', 
+    'torchaudio', 
+]
+
+
+setup(
+    name='schp', 
+    version='0.1',
+    packages=find_packages(),
+    install_requires=install_requires,
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+    author='BbChip0103',
+    author_email='bbchip13@gmail.com',
+    description='Make clothes segmentation image by SCHP',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/BbChip0103/Self-Correction-Human-Parsing',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
